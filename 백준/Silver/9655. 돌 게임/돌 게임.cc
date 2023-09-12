837124a429ab011333d9138f8a19f35140d8dee3
MIN <43802920+JunChangM@users.noreply.github.com>
2023-09-12 15:30:50 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int n;
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> n;
+	if (n% 2 == 1) cout << "SK";
+	else cout << "CY";
+	return 0;
+}	
