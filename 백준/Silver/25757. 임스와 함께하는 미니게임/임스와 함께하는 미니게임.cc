e8bd05b0c680539b9fc41542597bc544977300e3
MIN <43802920+JunChangM@users.noreply.github.com>
2023-09-14 10:12:27 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 108 ms, Memory: 13208 KB -BaekjoonHub

---
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <map>
+using namespace std;
+char c;
+int n, idx;
+string str;
+string input[100001];
+map<string, int> myMap;
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> n >> c;
+	while (n--) {
+		cin >> str;
+		if (myMap.find(str) == myMap.end()) {
+			myMap.insert(pair<string, int>(str, 1));
+		}
+		else {
+			myMap[str]++;
+		}
+	}
+	idx = myMap.size();
+	int ans = 0;
+	if (c == 'Y') ans = idx ;
+	else if (c == 'F') ans = idx / 2;
+	else if (c == 'O') ans = idx / 3;
+	cout << ans;
+	return 0;
+}	
