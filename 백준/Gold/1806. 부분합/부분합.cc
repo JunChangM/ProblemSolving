b5da5b4c93a1626da2e26a04e07b02b895a7d365
MIN <43802920+JunChangM@users.noreply.github.com>
2023-09-28 17:21:50 +0900

    Update 부분합.cc

--- 백준/Gold/1806. 부분합/부분합.cc
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -5,15 +5,10 @@ int input[100000];
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	cin >> n >> s;
-	for (int i = 0; i < n; i++) {
-		cin >> input[i];
-	}
+	for (int i = 0; i < n; i++) cin >> input[i];
 	int left = 0, right = 0, amount = 0, leng = 100001;
 	while (left <= right && right < n) {
-		 //cout << left << ' ' << right << ' ' << amount << endl;
-		if (amount < s) {
-			amount += input[right++];
-		}
+		if (amount < s) amount += input[right++];
 		else {
 			if (leng > right - left) leng = right - left;
 			amount -= input[left++];
