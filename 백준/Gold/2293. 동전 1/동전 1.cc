ccded64a8edd5904fc499159e9d7012e378ee5d6
MIN <43802920+JunChangM@users.noreply.github.com>
2023-11-08 09:50:14 +0900

    [Gold V] Title: 동전 1, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.cc
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -1,32 +1,15 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
-int n, k, t, ans;
-int coin[100];
-int value[10001];
+int n, k, t;
+int v[10001];
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	cin >> n >> k;
+	v[0] = 1;
 	for (int i = 0; i < n; i++) {
-		cin >> coin[i];
+		cin >> t;
+		for (int j = t; j <= k; j++) v[j] += v[j - t];
 	}
-	sort(coin, coin + n);
-	for (int i = 0; i < n; i++) {
-		int start = coin[i];
-
-		// 다른 코인 조합
-		for (int j = 1; j + start <= k; j++) {
-			value[j + start] += value[j];
-		}
-
-		// 단일 코인
-		for (int j = 1; start * j <= k; j++) {
-			value[start * j]++;
-		}
-		
-	}
-
-	cout << value[k];
-
+	cout << v[k];
 	return 0;
 }
