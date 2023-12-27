7bcd0dac376eabdeb1cb961b2e931cb9cd869465
MIN <43802920+JunChangM@users.noreply.github.com>
2023-12-28 08:28:19 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cout << "         ,r\'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |\n";
+
+	return 0;
+}
