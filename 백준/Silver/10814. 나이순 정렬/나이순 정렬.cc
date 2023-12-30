407c66ebc0cca29c74d8cd68ebc40af871c84a72
MIN <43802920+JunChangM@users.noreply.github.com>
2023-12-30 10:08:19 +0900

    Update 나이순 정렬.cc
    add explain

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -4,18 +4,18 @@
 using namespace std;
 int n;
 bool compare(pair<int, string> a, pair<int, string> b) {
-	return a.first < b.first;
+	return a.first < b.first; // compare 함수 반환시 true false를 바로 반환할 수 있다.
 }
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	cin >> n;
 
-	vector<pair<int, string>> v(n, { 0,"" });
+	vector<pair<int, string>> v(n, { 0,"" }); // v(크기, 초기값)
 	for (int i = 0; i < n; i++) {
 		cin >> v[i].first >> v[i].second;
 	}
 
-	stable_sort(v.begin(), v.end(), compare);
+	stable_sort(v.begin(), v.end(), compare); // 안정 정렬을 통해 중복되는 값들의 순서를 보장해주어야 한다.
 
 	for (int i = 0; i < n; i++) {
 		cout << v[i].first << ' ' << v[i].second << '\n';
