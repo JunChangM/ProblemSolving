350768085831d2282926faf3e9c4fc8d9d8b94ca
MIN <43802920+JunChangM@users.noreply.github.com>
2024-01-11 10:46:31 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	string s = "|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|\n";
+	cout << s;
+	return 0;
+}
