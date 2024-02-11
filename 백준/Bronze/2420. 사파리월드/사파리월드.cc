18e664b1fe0684853a5f53439c673240e3ad88da
MIN <43802920+JunChangM@users.noreply.github.com>
2024-02-12 08:34:44 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+long long n, m;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> n >> m;
+	cout << abs(n - m);
+
+	return 0;
+}
+
